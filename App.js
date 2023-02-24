5febe835f33bcbdd0fce8f12b403665ec25afea7
hasanbeker2 <hasanbeker@gmail.com>
2023-02-24 08:50:17 +0100

    new

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import { FlatList, Text, SafeAreaView, View, StyleSheet } from 'react-native';
+import menuItems from './menuItems.json';
+import React, { useState, useEffect } from 'react';
 
 export default App = () => {
+
+  const menu = menuItems.menu;
+
+  const Item = ({ name, price }) => (
+    <View style={menuStyles.innerContainer}>
+      <Text style={menuStyles.itemText}>{name}</Text>
+      <Text style={menuStyles.itemText}>{'$' + price}</Text>
+    </View>
+  );
+
+  const renderItem = ({ item }) => (
+    <Item name={item.title} price={item.price} />
+  );
+
   return (
     <SafeAreaView style={menuStyles.container}>
       <Text style={menuStyles.headerText}>Little Lemon Menu</Text>
+      <FlatList
+        data={menu}
+        keyExtractor={({ id }) => id}
+        renderItem={renderItem}
+      />
     </SafeAreaView>
   );
 };
@@ -11,6 +32,17 @@ export default App = () => {
 const menuStyles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  innerContainer: {
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    backgroundColor: '#495E57',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  itemText: {
+    color: '#F4CE14',
+    fontSize: 22,
   },
   headerText: {
     color: '#F4CE14',
